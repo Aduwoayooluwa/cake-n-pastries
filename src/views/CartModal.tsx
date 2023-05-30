111656243d2fe6e8146bac9da0825cfeb6c3bf8a
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2023-05-30 04:13:35 +0100

    fix/deleted 3d redundant model

--- src/views/CartModal.tsx
+++ src/views/CartModal.tsx
@@ -19,12 +19,6 @@ const CartModal = ({ isOpen, setIsOpen }: CartModalProps) => {
         setIsOpen(true);
     };
 
-    const cartItems = [
-        { id: "1", name: "Item 1", price: 10, quantity: 2 },
-        { id: "2", name: "Item 2", price: 15, quantity: 1 },
-        { id: "3", name: "Item 3", price: 5, quantity: 4 },
-        ];
-
     return (
         <Box>
         <Button bg="none" onClick={handleOpen}>
