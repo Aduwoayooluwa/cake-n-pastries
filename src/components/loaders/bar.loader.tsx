713e738ffa643add1146957f640df69a7e208d05
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2023-05-30 20:06:25 +0100

    removed navigation to a new page

--- src/components/loaders/bar.loader.tsx
+++ src/components/loaders/bar.loader.tsx
@@ -1,40 +1,60 @@
-import { Box, Flex } from '@chakra-ui/react'
-import { useRouter } from 'next/router'
-import React, { useEffect, useState } from 'react'
+import { Box, Flex } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import gsap from 'gsap';
 
-type Props = {}
+type Props = {
+    setIsLoadingComplete: any
+};
 
-const BarLoader = (props: Props) => {
-    const [width, setWidth] = useState(0)
-    const router = useRouter()
+const BarLoader = ({setIsLoadingComplete }: Props) => {
+    const [width, setWidth] = useState(0);
+    const router = useRouter();
 
     useEffect(() => {
+        const tl = gsap.timeline();
+
         const interval = setInterval(() => {
-        setWidth((prev) => prev + 10)
-        }, 500)
+        setWidth((prev) => prev + 10);
+        }, 500);
 
         if (width >= 100) {
-            clearInterval(interval)
-            router.push("/home")
+        clearInterval(interval);
+        tl.to('.loader-bar', { duration: 0.5, scaleX: 0, ease: 'power2.in' });
+        tl.to('.loader', { duration: 0.3, opacity: 0, ease: 'power2.in' });
+        tl.to('.loader', { duration: 0.1, height: 0, display: 'none', ease: 'power2.out' });
+        //router.push('/home');
+        setIsLoadingComplete(true);
         }
 
         return () => {
-        clearInterval(interval)
-        }
-    }, [width, router])
+        clearInterval(interval);
+        };
+    }, [width, setIsLoadingComplete]);
 
     return (
-
-    <Flex paddingX="20px" direction="column" align="center" width="100%" bg="whiteAlpha.500">
+        <Flex
+        paddingX={{ base: '10px', md: '20px' }}
+        direction="column"
+        align="center"
+        width="100%"
+        
+        bg="whiteAlpha.500"
+        className="loader"
+        >
         <Box
-            height="24px"
-            bg="green.300"
-            width={`${width}%`}
-            transition="none"
+            height="8px"
+            border={"1px solid black"}
+            borderRadius={"20px"}
+        bg="pink.300"
+        width={{ base: `${width}%`, md: '100%' }}
+        transition="none"
+        py={"10px"}
+        className="loader-bar"
         />
         {width}%
     </Flex>
-    )
-}
+  );
+};
 
-export default BarLoader
+export default BarLoader;
--- src/views/Preloader.tsx
+++ src/views/Preloader.tsx
@@ -6,8 +6,10 @@ import Image from "next/image";
 import CookingGif from "@/assets/images/cooking.gif"
 import gsap from 'gsap'
 import BarLoader from "@/components/loaders/bar.loader";
+import LandingPage from "./LandingPage";
 
 const Bubble = ({ size }: any) => {
+    const [isLoadingComplete, setIsLoadingComplete] = useState(false);
     const words = [
         "cooking your desires",
         "it won't be long"
@@ -32,13 +34,23 @@ const Bubble = ({ size }: any) => {
 
 
     return (
-        <Flex direction="column" width={"100%"} align="center" position={"absolute"} bg="whiteAlpha.200">
-            
-            <Image data-download="none" src={CookingGif} alt="cooking" width={400} height={400}/>
-            <Text className="box" fontWeight={"semibold"}>{words[0]}...</Text>
-            Cooking your dreams..
-            <BarLoader />
-        </Flex>
+        <>
+            {
+                !isLoadingComplete && (
+                    <Flex direction="column" width={"100%"} align="center" position={"absolute"} bg="whiteAlpha.200">
+                
+                <Image data-download="none" src={CookingGif} alt="cooking" width={400} height={400}/>
+                <Text className="box" fontWeight={"semibold"}>{words[0]}...</Text>
+                Cooking your dreams..
+                <BarLoader setIsLoadingComplete={setIsLoadingComplete}/>
+            </Flex>
+                )
+            }
+
+            { isLoadingComplete && (<LandingPage />)}
+        </>
+        
+        
     );
 };
 
